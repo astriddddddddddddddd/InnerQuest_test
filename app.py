e53f3d1e80454ca40c7dfde2ea6abd6a02134625
astriddddddddddddddd <161142783+astriddddddddddddddd@users.noreply.github.com>
2024-09-25 10:53:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import os
 import openai
 
 #gemini
-#genai.configure(api_key='AIzaSyDMG6KRWhCJO1yDk1okerJcBPm4pwlCwy0')
+#genai.configure(api_key='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
+')
 
 #OpenAI
 openai.api_key = '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'
